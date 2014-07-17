97481c21fe9f104b67f28257192cff8100f3ba97
Sanjeev Sekar & J.J. Idt <student@epicodus.com>
2014-07-16 21:14:50 -0700

    made all triangle's displayed tilt right to stay in the viewing area, added css styling,added functionality to automatically calculate the area of the triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
+var currentTri = "";
 $(document).ready(function() {
   $("#selector").submit(function(event){
+    event.preventDefault();
     var inputtedSide1 = parseInt($("#sideA").val());
-    var inputtedSide2 = parseInt($("#sideB").val());
-    var inputtedSide3 = parseInt($("#sideC").val());
+    var temp;
+
+    if (parseInt($("#sideB").val()) < parseInt($("#sideC").val())) {
+      var inputtedSide3 = parseInt($("#sideB").val());
+      var inputtedSide2 = parseInt($("#sideC").val());
+    } else {
+      var inputtedSide2 = parseInt($("#sideB").val());
+      var inputtedSide3 = parseInt($("#sideC").val());
+    }
 
     var newTriangle = {
       side1: inputtedSide1,
@@ -19,8 +28,18 @@ $(document).ready(function() {
             } else {
               return "scalene";
             }
+      },
+      area: function(){
+        var a = this.side1;
+        var b = this.side2;
+        var c = this.side3;
+        var p = (a + b + c)/2;
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
       }
     }
+
+    $("#numbers").empty().append(newTriangle.area().toFixed(2))
+
     if (!newTriangle.triangleCheck()) {
       alert("Please Enter a Valid Triangle");
     } else {
@@ -29,42 +48,71 @@ $(document).ready(function() {
     }
     $("input").val("");
     //$("#show").show();
-    var right = (newTriangle.side1*10).toString();
+    var right =((newTriangle.side1 + newTriangle.side2 + newTriangle.side3) *10).toString();
     var up =(newTriangle.side3*10).toString();
+    var realign = 0;
+
+    // if (newTriangle.type() === "scalene" && newTriangle.side1 < newTriangle.side2) {
+    //   alert("scalene");
+    //   right = ((newTriangle.side1+newTriangle.side2)*10).toString();
+    //   realign = right/2;
+    // } else {
+    //   right = (newTriangle.side1*20).toString()
+    //   realign = right/20;
+    // }
+
+    // if (newTriangle.side2>newTriangle.side3) {
+    //   var temp = newTriangle.side3
+    //   newTriangle.side3=newTriangle.side2;
+    //   newTriangle.side2= temp;
+    // }
 
     $("#canvas-box").empty().append("<canvas id='myCanvas' width=" + right + " height=" + up +"></canvas>")
+
+    var a2 = newTriangle.side1*10;
+    var b2 = newTriangle.side2*10;
+    var c2 = newTriangle.side3*10;
+
 
     var triangleDraw = function(a, b, c){
       var canvas=document.getElementById('myCanvas');
       var ctx=canvas.getContext('2d');
       var x = (Math.pow(b, 2) + Math.pow(a,2) - Math.pow(c, 2))/(2*a);
       var y = Math.sqrt(Math.pow(b, 2) - Math.pow(x, 2));
-        ctx.clearRect ( 1000 , 1000 , 1000 , 1000 )
-        ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(a, 0);
-        ctx.lineTo(x,y);
-        ctx.fill();
-      event.preventDefault();
+        // ctx.clearRect ( 1000 , 1000 , 1000 , 1000 )
+      ctx.beginPath();
+      ctx.moveTo(realign,0);
+      ctx.lineTo(a + realign, 0);
+      ctx.lineTo(x + realign,y);
+      ctx.lineTo(realign,0);
+      ctx.globalAlpha = .5;
+      ctx.fillStyle = 'orange';
+      ctx.fill();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'black';
+      ctx.stroke();
+
     };
-      var a = newTriangle.side1*10;
-      var b = newTriangle.side2*10;
-      var c = newTriangle.side3*10;
 
-    triangleDraw(a, b, c);
+    triangleDraw(a2, b2, c2);
 
-    $("li").off("mouseover").on("mouseover", function(event){
+    $("li").on("mouseover", function(){
       $("li").removeClass("selected");
       $(this).addClass("selected");
+      //currentTri.animate({"font-size": "15px"})
+      //alert(currentTri);
       var tempArr = $(this).text().split(",");
       var a1 = tempArr[0]*10;
       var b1 = tempArr[1]*10;
       var c1 = tempArr[2]*10;
-      event.preventDefault;
-      var right1 = a1.toString();
+      newTriangle.side1= parseInt(tempArr[0]);
+      newTriangle.side2= parseInt(tempArr[1]);
+      newTriangle.side3= parseInt(tempArr[2]);
+      var right1 = (a1+b1+c1).toString();
       var up1 =c1.toString();
       $("#canvas-box").empty().append("<canvas id='myCanvas' width=" + right1 + " height=" + up1 +"></canvas>")
       triangleDraw(a1, b1, c1);
+      $("#numbers").empty().append(newTriangle.area().toFixed(2))
     });
   });
 });
